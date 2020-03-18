41f02f0391d3d6ef0f3656f905baa816ff216b46
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-18 16:56:47 +0000

    finishing touches

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 /////////////////////////
 // Challenge
 
+/*
 (function() {
     function Question(question, answers, correct) {
     this.question = question;
@@ -51,6 +52,89 @@
 
     questions[n].checkAnswer(answer);
 })();
+*/
+
+(function() {
+    function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion =  function() {
+        console.log(this.question);
+
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
+
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+        
+        if (ans === this.correct) {
+            console.log('Correct');
+            sc = callback(true);
+        } else {
+            console.log('Incorrect. Try again');
+            sc = callback(false);
+        }
+        
+        this.displayScore(sc);
+    }
+    
+    Question.prototype.displayScore = function(score) {
+        console.log('Your current score is :' + score);
+        console.log('----------------------------');
+    }
+
+    var q1 = new Question('Is JavaScript the best language?',
+                         ['Yes', 'No'],
+                         0
+                         );
+
+    var q2 = new Question('What is the name of your employer',
+                         ['Lucia', 'Paul', 'Phil'],
+                         1
+                         );
+
+    var q3 = new Question('What does best describe coding?',
+                         ['Easy', 'Hard', 'Boring', 'Great'],
+                         3
+                         );
+    
+    var questions = [q1, q2, q3];
+    
+    function score() {
+        var sc = 0;
+        return function(correct) {
+            if (correct) {
+                sc++;
+            } 
+            return sc;
+        }
+    }
+    
+    var keppScore = score();
+    
+    
+    function nextQuestion() {
+
+        var n = Math.floor(Math.random() * questions.length);
+
+        questions[n].displayQuestion();
+
+        var answer = prompt('Please select the correct answer');
+
+        if (answer !== 'exit') {
+            questions[n].checkAnswer(parseInt(answer), keppScore);
+        
+            nextQuestion();
+        }
+    }
+    nextQuestion();
+})();
+
 
 
 
