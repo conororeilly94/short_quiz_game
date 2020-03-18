2232174899df96460d3862d46ab0a1e735113121
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-18 16:25:30 +0000

    able to ask and answer question

--- script.js
+++ script.js
@@ -3,17 +3,54 @@
 /////////////////////////
 // Challenge
 
+(function() {
+    function Question(question, answers, correct) {
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+    }
 
+    Question.prototype.displayQuestion =  function() {
+        console.log(this.question);
 
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    }
 
+    Question.prototype.checkAnswer = function(ans) {
+        if (ans === this.correct) {
+            console.log('Correct');
+        } else {
+            console.log('Incorrect. Try again');
+        }
+    }
 
+    var q1 = new Question('Is JavaScript the best language?',
+                         ['Yes', 'No'],
+                         0
+                         );
 
+    var q2 = new Question('What is the name of your employer',
+                         ['Lucia', 'Paul', 'Phil'],
+                         1
+                         );
 
+    var q3 = new Question('What does best describe coding?',
+                         ['Easy', 'Hard', 'Boring', 'Great'],
+                         3
+                         );
 
+    var questions = [q1, q2, q3];
 
+    var n = Math.floor(Math.random() * questions.length);
 
+    questions[n].displayQuestion();
 
+    var answer = parseInt(prompt('Please select the correct answer'));
 
+    questions[n].checkAnswer(answer);
+})();
 
 
 
